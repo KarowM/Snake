58ab417a4f1466bb643eff3eccd4eec9e23151a4
karow <karow.foxx@gmail.com>
2020-10-02 22:36:26 +0100

    added spawner to spawn food every 4 seconds

---
+++ Assets/FoodSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodSpawner : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject food;
+
+    [SerializeField]
+    private Transform borderTop;
+    [SerializeField]
+    private Transform borderBottom;
+    [SerializeField]
+    private Transform borderLeft;
+    [SerializeField]
+    private Transform borderRight;
+
+    void Start() {
+        InvokeRepeating("SpawnFood", 3, 4);
+    }
+
+    void SpawnFood() {
+        int x = (int)Random.Range(borderLeft.position.x, borderRight.position.x);
+        int y = (int)Random.Range(borderTop.position.y, borderBottom.position.y);
+
+        Instantiate(food, new Vector2(x, y), Quaternion.identity);
+    }
+}
